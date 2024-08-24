3ba52a7f40d3f2cb2d0c0c3285418a07b8419803
Ayla Natalia Martins Ferreira <aylantalia08@hotmail.com>
2024-08-24 14:50:39 -0300

    laco- repeticao- for

---
+++ Models/Calculadora.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exemplo_fundamentos.Models
+{
+    public class Calculadora
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"{x} + {y} = {x +y}");
+        }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} * {y} = {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+
+        public void Potencia(int x, int y)
+        {   
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"{x}^{y} = {pot}");
+        }
+
+        public void Seno(double angulo)
+        {   
+            double radiano = angulo * Math.PI;
+            double sen = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo} = {Math.Round(sen, 4)}");
+        }
+
+        public void Conseno(double angulo)
+        {   
+            double radiano = angulo * Math.PI;
+            double cos = Math.Cos(radiano);
+            Console.WriteLine($"Seno de {angulo} = {Math.Round(cos, 4)}");
+        }
+
+        public void Tangente(double angulo)
+        {   
+            double radiano = angulo * Math.PI;
+            double tan = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo} = {Math.Round(tan, 4)}");
+        }
+
+        public void RaizQuadrada(double x)
+        {
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"Raiz quadrada de {x} = {raiz}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿
+int quantidadeEmEstoque = 10;
+int quantidadeCompra = 0;
+
+//exemplo de condicional 
+
+// if(quantidadeEmEstoque >= quantidadeCompra && quantidadeCompra > 0)
+// {
+//     Console.WriteLine("Venda realizada com sucesso");
+// }
+// else if(quantidadeCompra == 0)
+// {
+//   Console.WriteLine("Venda inválida");
+// }
+// else
+// {
+//    Console.WriteLine("Venda não realizada por falta de estoque");
+// }
+
+
+// Console.WriteLine("Digite uma letra");
+// string letra = Console.ReadLine();
+
+// // exemplo switch case
+
+// switch(letra)
+// {
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//     Console.WriteLine("Vogal");
+//     break;
+
+//     default:
+//     Console.WriteLine("Consoante");
+
+//     break;
+// }
+
+
+// exemplo for
+
+int numero = 5;
+
+for(int contador = 0; contador <= 10; contador++)
+{
+  Console.WriteLine($"{numero} * {contador} = {numero * contador}");
+}
