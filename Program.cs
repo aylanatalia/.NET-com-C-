408ccc508d9fb3f6d0105b4a421f5475a49d75af
Ayla Natalia Martins Ferreira <aylantalia08@hotmail.com>
2024-08-25 10:44:22 -0300

    menu-interativo-suando-while

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using exemplo_fundamentos.Models;
+
 int quantidadeEmEstoque = 10;
 int quantidadeCompra = 0;
 
@@ -42,7 +43,7 @@ int quantidadeCompra = 0;
 
 // exemplo for
 
-int numero = 5;
+//int numero = 5;
 
 // for(int contador = 0; contador <= 10; contador++)
 // {
@@ -51,15 +52,67 @@ int numero = 5;
 
 // exemplo while
 
-int contador = 0;
+// int contador = 0;
 
-while(contador <=10)
+// while(contador <=10)
+// {
+//   Console.WriteLine($"{numero} * {contador} = {numero * contador}");
+//   contador++;
+
+//   if(contador == 5)
+//   {
+//     break;
+//   }
+// }
+
+//exemplo do while 
+
+// int soma = 0, numero = 0;
+
+// do
+// {
+//   Console.WriteLine("Digite um número(0 para parar)");
+// numero = Convert.ToInt32(Console.ReadLine());
+// soma += numero;
+// } while(numero != 0);
+
+// Console.WriteLine($"Total da soma dos numeros digitados é {soma}");
+
+string opcao;
+bool exibirMenu = true;
+
+while(exibirMenu)
 {
-  Console.WriteLine($"{numero} * {contador} = {numero * contador}");
-  contador++;
+  Console.Clear();
+  Console.WriteLine("Digite sua opção");
+  Console.WriteLine("1 - Cadastrar cliente");
+  Console.WriteLine("2 - Buscar cliente");
+  Console.WriteLine("3 - Apagar cliente");
+  Console.WriteLine("4 - Encerrar");
 
-  if(contador == 5)
+  opcao = Console.ReadLine();
+
+  switch(opcao)
   {
-    break;
+    case "1":
+      Console.WriteLine("Cadastro de cliente");
+      break;
+
+    case "2":
+      Console.WriteLine("Busca de cliente");
+      break;
+
+    case "3":
+      Console.WriteLine("Apaga o cliente");
+      break;
+
+    case "4":
+      Console.WriteLine("Encerra");
+      exibirMenu = false;
+      break;
+
+    default:
+      Console.WriteLine("Opção invalida");
+      break;
   }
 }
